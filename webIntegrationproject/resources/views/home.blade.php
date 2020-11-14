837ae4977cbb2c86bb0b99e174e7a6acac3ed2a2
iliassboutechdat <iliass.boutechdat@student.ehb.be>
2020-11-14 03:51:49 +0100

    first commit

---
+++ webIntegrationproject/resources/views/home.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="\images\Pdp4.PNG" class="rounded-circle" style="width:140px;height:150px;">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Iliassma212</h1></div>
+          <div class="d-flex">
+          <div class="pr-5"><strong>0</strong> Posts</div>
+          <div class="pr-5"><strong>0</strong> Followers</div>
+          <div class="pr-5"><strong>0</strong> Following</div>
+          </div>
+          <div class="pt-4">
+              <img src="\svg\calendar.svg" style="max-height: 40px;">
+            <strong>October</strong> 27th</div>
+            <div>
+              <img src="\svg\bio.svg" style="max-height: 40px;">
+              S𝕖𝕦𝕝 𝕝𝕖𝕤 𝕡𝕖𝕣𝕤𝕠𝕟𝕟𝕖𝕤 𝕡𝕣𝕠𝕔𝕙𝕖 𝕕𝕖 𝕥𝕠𝕚 𝕡𝕖𝕦𝕧𝕖𝕟𝕥 𝕥𝕖 𝕞é𝕥𝕣𝕖 𝕦𝕟 𝕔𝕠𝕦𝕥𝕖𝕒𝕦𝕩 𝕕𝕒𝕟𝕤 𝕝𝕖 𝕕𝕠𝕤</div>
+              <div class="pb-5">
+              <img src="\svg\search.svg" style="max-height: 40px;">
+              <a href="#">https://twitter.com/Iliassma212</a></div>
+          </div>  
+        </div>
+    </div>
+    <div class="container-fluid d-flex mt-5  ml-5 ">
+        <div class="post col-4"><p class="post-text pb-1">Ma nouvelle voiture ah chien!</p><br>
+            <img src="https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/1-mercedes-amg-gt63-fourdoor-coupe-2019-uk-fd-hero-front.jpg?itok=ouf9g20f" class="w-100"></div>
+        <div class="post col-4"><p class="post-text pb-1">Oujda ma ville</p><br>
+        <img src="https://media.istockphoto.com/photos/oujda-city-and-mosque-picture-id491669482?k=6&m=491669482&s=612x612&w=0&h=4Q8_HKrFD-IkIiBnLaVUXhXYEFb94TIUdXbQz-eHDGY=" class="w-100"></div>
+        <div class="post col-4"><p class="post-text pb-1">Tommy Shelby</p><br>
+        <img src="https://thumbor.pijper.io/nZuED_wKBRY-uSIx0r8sUpf8D70=/1290x726/top/https://cdn.pijper.io/2019/11/Nuki2KdfLv1572601453.jpeg" class="w-100"></div>
+    </div>
+</div>
+@endsection
