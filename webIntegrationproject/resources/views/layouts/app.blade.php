0777b65439dac03dd9eb356cea9e5f1409f2c39f
iliassboutechdat <iliass.boutechdat@student.ehb.be>
2020-11-15 22:04:37 +0100

    Sidebar added

--- webIntegrationproject/resources/views/layouts/app.blade.php
+++ webIntegrationproject/resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,16 +20,28 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <div><img src="\svg\Tweetogram.svg" style="max-height: 120px;" class="pr-3"></div>
-                    <div>TweetoGram</div>
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+    </div>
+    <div>
+        <div class="pl-2">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <div><img src="\svg\Tweetogram.svg" style="max-height: 120px;" class="pr-3"></div>
+            <div>TweetoGram</div>
+        </a>
+        </div>
+        <nav id="sidebar">
+            <!-- Sidebar Header -->
+            <div class="sidebar-header">
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -38,54 +51,66 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="list-unstyled components ">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('posts.create') }}">New TG</a>
-                                </li>
+                        @if (Route::has('login'))
+                        <li class="nav-item list-unstyled components">
+                            <a class="nav-link" href="{{ route('posts.create') }}">New TG</a>
+                        </li>
+                        @endif
+                        @if (Route::has('login'))
+                        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                            @auth
+                            <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Profile</a>
+                            @else
+                            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
                             @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
                             @endif
+                        </div>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-                                <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+                            <ul class="navbar-nav ml-auto list-unstyled components ">
+                                <!-- Authentication Links -->
+                                <li class="nav-item">
+                                <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Profile</a>
+                                </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('posts.create') }}">New TG</a>
                                 </li>
                                 <li class="nav-item">
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-                                    </li>
-                                    </ul>
+                                            {{ __('Logout') }}
+                                        </a>
+                                </li>
+                            </ul>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
                 </div>
+                </li>
+                @endguest
+                </ul>
             </div>
-        </nav>
+    </div>
+    </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+    <main class="py-4">
+        @yield('content')
+    </main>
     </div>
 </body>
+
 </html>
--- webIntegrationproject/resources/views/posts/index.blade.php
+++ webIntegrationproject/resources/views/posts/index.blade.php
@@ -2,22 +2,8 @@
 
 @section('content')
 
-<div style="background-image: url('/images/bluegradientbackground.jpg')" id="background">
-    <div
-        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        @if (Route::has('login'))
-        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            @auth
-            <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Profile</a>
-            @else
-            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
 
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-            @endif
-            @endif
-        </div>
-        @endif
+
 
         <div class="container">
             <form method="POST" action="{{ route('posts.store')}}">
@@ -62,7 +48,6 @@
                     </div>
                 </div>
             </div>
-        </div>
     </div>
 </div>
 
